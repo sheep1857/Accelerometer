01c606942975f9bb694ecbbf09c8753d04243abf
Miki Kimura <mikimucun0802@gmail.com>
2023-02-18 01:28:14 +0900

    Initial Commit

---
+++ Accelerometer/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Accelerometer
+//
+//  Created by 木村美希 on 2023/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func unwindToViewController(segue: UIStoryboardSegue) {
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
